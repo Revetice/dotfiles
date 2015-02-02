3e3871d9d7bd2f2c54e4e9bb555ca9964021313e
yilin.w <revetice@gmail.com>
2015-02-02 14:36:17 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,35 @@
 #!/usr/bin/env bash
+
+# vim
+sudo apt-get install gvim
+# for vim You Complete Me
+sudo apt-get install cmake
+
+# python
+sudo apt-get install python
+sudo apt-get install python-dev
+
+pip install virtualenv
+easy_install -U Sphinx
+
+# git
+sudo apt-get install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+sudo apt-get install git
+
+# Utilities
+sudo apt-get install unity-tweak-tool
+sudo apt-get install qalculate
+sudo apt-get install guake
+sudo apt-get install tree
+sudo apt-get install htop
+sudo apt-get install xclip
+sudo apt-get install meld
+# chinese font
+sudo apt-get install fonts-wqy-zenhei
+
+# Samba
+# sudo apt-get install samba
+# sudo apt-get install cifs-utils
+
+# Open SSH
+# sudo apt-get install openssh-server
