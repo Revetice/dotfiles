feb0a8a24d807976b5c4ae5be167879c66f32d40
Yilin <yilin@Yilinde-MacBook-Pro.local>
2019-04-17 16:59:39 +0800

    update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -8,7 +8,7 @@
 	("04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
  '(package-selected-packages
    (quote
-	(doom-themes doom-modeline company rainbow-delimiters expand-region flycheck solarized-theme multiple-cursors zenburn-theme swiper magit evil projectile helm dracula-theme))))
+	(use-package doom-themes doom-modeline company rainbow-delimiters expand-region flycheck solarized-theme multiple-cursors zenburn-theme swiper magit evil projectile helm dracula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,79 +18,120 @@ There are two things you can do about this warning:
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
-;; fix string-trim error
-(require 'subr-x)
+(dolist (package '(use-package))
+   (unless (package-installed-p package)
+       (package-install package)))
 
-;;
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  ;; (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
 
-;;
-(require 'helm)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(helm-mode 1)
 
-;;
-(global-set-key "\C-s" 'swiper)
+(use-package org
+  :ensure t
+  :init
+  (setq org-log-done t)
+  :config
+  (define-key global-map "\C-cl" 'org-store-link)
+  (define-key global-map "\C-ca" 'org-agenda))
 
-;;
-(require 'evil)
-(setq evil-normal-state-tag   (propertize " <N> " 'face '((:background "DarkGoldenrod2" :foreground "black")))
-      evil-emacs-state-tag    (propertize " <E> " 'face '((:background "SkyBlue2"       :foreground "black")))
-      evil-insert-state-tag   (propertize " <I> " 'face '((:background "chartreuse3"    :foreground "black")))
-      evil-replace-state-tag  (propertize " <R> " 'face '((:background "chocolate"      :foreground "black")))
-      evil-motion-state-tag   (propertize " <M> " 'face '((:background "plum3"          :foreground "black")))
-      evil-visual-state-tag   (propertize " <V> " 'face '((:background "gray"           :foreground "black")))
-      evil-operator-state-tag (propertize " <O> " 'face '((:background "sandy brown"    :foreground "black"))))
-(define-key evil-emacs-state-map (kbd "C-z") 'evil-force-normal-state)
-(define-key evil-normal-state-map (kbd "M-.") 'xref-find-definitions)
-(setq evil-normal-state-cursor '("orange")
-      evil-emacs-state-cursor '("cyan"))
-;; (setq evil-search-module 'evil-search)
-(setq-default evil-symbol-word-search t)
-(evil-mode 1)
+(use-package helm
+  :ensure t
+  :bind (("M-x" . helm-M-x)
+		 ("C-x C-f" . helm-find-files)
+		 ("C-x r b" . helm-filtered-bookmarks))
+  :config
+  (helm-mode 1))
 
-;;
-(require 'projectile)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
-(setq projectile-completion-system 'helm)
+(use-package projectile
+  :ensure t
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-completion-system 'helm)
+  :config
+  (projectile-mode 1))
 
-;;
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
+(use-package expand-region
+  :ensure t
+  :config
+  (global-set-key (kbd "C-=") 'er/expand-region)
+  (global-set-key (kbd "C--") 'er/contract-region))
+
+(use-package company
+  :ensure t
+  :init
+  (setq company-idle-delay 0)
+  (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (add-to-list 'company-backends '(company-capf company-dabbrev)))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-normal-state-tag   (propertize " <N> " 'face '((:background "DarkGoldenrod2" :foreground "black")))
+		evil-emacs-state-tag    (propertize " <E> " 'face '((:background "SkyBlue2"       :foreground "black")))
+		evil-insert-state-tag   (propertize " <I> " 'face '((:background "chartreuse3"    :foreground "black")))
+		evil-replace-state-tag  (propertize " <R> " 'face '((:background "chocolate"      :foreground "black")))
+		evil-motion-state-tag   (propertize " <M> " 'face '((:background "plum3"          :foreground "black")))
+		evil-visual-state-tag   (propertize " <V> " 'face '((:background "gray"           :foreground "black")))
+		evil-operator-state-tag (propertize " <O> " 'face '((:background "sandy brown"    :foreground "black"))))
+  (setq evil-normal-state-cursor '("orange")
+  		evil-emacs-state-cursor '("cyan"))
+  (setq-default evil-symbol-word-search t)
+  :config
+  (define-key evil-emacs-state-map (kbd "C-z") 'evil-force-normal-state)
+  (define-key evil-normal-state-map (kbd "M-.") 'xref-find-definitions)
+  (evil-mode 1))
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons
+  :ensure t)
+
+(use-package doom-modeline
+  :ensure t
+  :init
+  (setq doom-modeline-buffer-file-name-style 'file-name)
+  :config
+  (doom-modeline-mode 1))
+
+(use-package doom-themes
+  :ensure t
+  :init
+  (setq doom-dracula-brighter-modeline t)
+  :config
+  (load-theme 'doom-dracula t))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (global-set-key "\C-s" 'swiper))
+
+;; (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;;
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+;; built in
 
-;;
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C--") 'er/contract-region)
+;; fix string-trim error
+(use-package subr-x)
 
-;;
-(global-flycheck-mode)
-
-;;
-;; (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
-;;
-(require 'company)
-(setq company-idle-delay 0)
-(add-hook 'after-init-hook 'global-company-mode)
-(add-to-list 'company-backends '(company-capf company-dabbrev))
-
-;;
-(toggle-frame-fullscreen)
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 
@@ -101,41 +142,29 @@ There are two things you can do about this warning:
 (setq display-time-24hr-format t)
 (display-time-mode t)
 
-(setq-default tab-width 4)
-
 (setq show-paren-delay 0)
 (show-paren-mode 1)
 
 (which-function-mode 1)
 
+(toggle-frame-fullscreen)
+
 (set-face-attribute 'default nil :height 180)
 
+(setq-default tab-width 4)
 (setq inhibit-splash-screen t)
 (setq vc-follow-symlinks nil)
 (setq scroll-error-top-bottom 'true)
 (setq lazy-highlight-cleanup nil)
 (setq lazy-highlight-initial-delay 0)
 
-;;
+;; custom function
 (defun find-user-init-file ()
   (interactive)
   (find-file-other-window user-init-file))
+
 (global-set-key (kbd "C-c I") 'find-user-init-file)
 (global-set-key (kbd "<f12>") 'find-user-init-file)
-
-;;
-(require 'all-the-icons)
-;; M-x all-the-icons-install-fonts
-
-;;
-(require 'doom-modeline)
-(setq doom-modeline-buffer-file-name-style 'file-name)
-(doom-modeline-mode 1)
-
-;;
-(require 'doom-themes)
-;; (load-theme 'doom-one t)
-(load-theme 'doom-dracula t)
 
 ;;
 ;; (load-theme 'zenburn t)
